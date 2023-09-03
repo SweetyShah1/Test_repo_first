7c865cf0c9332df213be13d824125f4de65b1418
Lifecell <Lifecell@LCLAP2228.lifecell.in>
2023-09-03 15:45:26 +0530

    My first commit

---
+++ Selenium_test/src/Test/Student.java
@@ -0,0 +1,32 @@
+package Test;
+
+public class Student 
+{
+	int RollNO, Age;
+
+	public void Display1()
+	{
+		System.out.println("Welcome to all of us ");
+	}
+
+	public void Display2()
+	{
+		System.out.println("Automation is very easy ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Student st1= new Student();
+			st1.Age= 25;
+			st1.RollNO= 42;
+			
+			System.out.println("Roll no is "+ st1.RollNO);
+			System.out.println("Age is "+ st1.Age);
+			
+			st1.Display1();
+			st1.Display2();
+	}
+
+}
+
